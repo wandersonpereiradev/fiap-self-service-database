295325cb9fbf765909324113c2b48ea0dc94d1dc
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-09-25 20:46:18 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-unique-fiap-k8s"
+    bucket         = "terraform-state-bucket-unique-fiap-database"
     key            = "terraform/state"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks-fiap-k8s"
+    dynamodb_table = "terraform-locks-fiap-database"
   }
 }
