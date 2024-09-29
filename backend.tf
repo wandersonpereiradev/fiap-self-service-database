ea451698db6215079f2e0743e3929aaa34572c46
Wanderson <wanderson.p.ayres@gmail.com>
2024-09-29 12:45:12 -0300

    Update backend.tf
    Ajuste para rodar o S3

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-unique-fiap-database"
+    bucket         = "terraform-state-bucket-unique-fiap"
     key            = "terraform/state"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks-fiap-database"
+    dynamodb_table = "terraform-locks-fiap"
   }
 }
